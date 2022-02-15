58fc1757295e48389832b564c2c45294274c7c91
Ana Lastoviria <ana.lastoviria@zendesk.com>
2022-02-15 11:39:33 +1100

    wip connecting to postgres

--- src/main/java/com/sensors/sensors/MainVerticle.java
+++ src/main/java/com/sensors/sensors/MainVerticle.java
@@ -2,10 +2,18 @@ package com.sensors.sensors;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -35,8 +43,54 @@ public class MainVerticle extends AbstractVerticle {
         System.out.println("http server running: " + httpPort);
         startPromise.complete();
       })
-      .onFailure(startPromise::fail)
+      .onFailure(startPromise::fail);
   }
 
+  private void getAllData(RoutingContext context){
+    System.out.println("Requesting all data from " + context.request().remoteAddress());
+    String query = "select * from temperature_records";
+    pgPool.preparedQuery(query)
+      .execute()
+      .onSuccess(rows ->{
+        JsonArray array = new JsonArray();
+        for (Row row : rows) {
+          array.add(new JsonObject()
+            .put("uuid", row.getString("uuid"))
+            .put("temperature", row.getDouble("value"))
+            .put("timestamp", row.getTemporal("tstamp").toString()));
+        }
+        context.response()
+          .putHeader("Content-Type", "application/json")
+          .end(new JsonObject().put("data", array).encode());
+      })
+      .onFailure(failure ->{
+        System.out.println("Woops " + failure);
+        context.fail(500);
+      });
+  }
+
+  private void getLastFiveMinutes(RoutingContext context){
+
+  }
+
+  private void getData(RoutingContext context){
+
+  }
+
+  private void recordTemperature(Message<JsonObject> message){
+
+  }
+
+
+  public static void main(String[] args) {
+    Vertx.clusteredVertx(new VertxOptions())
+      .onSuccess(vertx -> {
+        vertx.deployVerticle(new MainVerticle());
+        System.out.println("Running");
+      })
+      .onFailure(failure -> {
+        System.out.println("Not running: " + failure);
+      });
+  }
 
 }
